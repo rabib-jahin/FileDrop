b2e89e796633182f40ab13508ac0a57cd61d18d8
rabib-jahin <rabibjahin2000@gmail.com>
2021-02-06 12:26:04 +0600

    cors

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const express=require('express');
 const app=express();
 
 const path=require('path')
-
+const cors=require('cors')
 const PORT=process.env.PORT||3000;
 app.use(express.static('public'))
+app.use(express.json())
 const connectDB=require('./config/db');
 connectDB();
 
+const corsOptions={
+
+    origin:process.env.ALLOWED.split(',')
+}
+app.use(cors(corsOptions))
 app.set('views',path.join(__dirname,'/views'))
 app.set('view engine','ejs')
 
